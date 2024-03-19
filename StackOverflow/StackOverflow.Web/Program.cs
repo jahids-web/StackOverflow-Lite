d01bc33daf868daece43158684f3356d55ae106d
jahids-web <jahiduddin922@gmail.com>
2024-03-19 17:12:07 +0600

    Add Some code

--- StackOverflow/StackOverflow.Web/Program.cs
+++ StackOverflow/StackOverflow.Web/Program.cs
@@ -14,11 +14,12 @@ namespace StackOverflow.Web
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             var app = builder.Build();
 
