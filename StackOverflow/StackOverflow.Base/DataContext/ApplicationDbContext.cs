91fb00f8998dfd37edfdb5ace4f358ede0051229
jahids-web <jahiduddin922@gmail.com>
2024-03-22 15:25:09 +0600

    Create a New Class

---
+++ StackOverflow/StackOverflow.Base/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace StackOverflow.Base.DataContext
+{
+    public class ApplicationDbContext(DbContextOptions options) : base(Options)
+    {
+
+    }
+}
