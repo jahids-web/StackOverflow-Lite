d91176180cfaf383ee7f8f62bb3f7cc926d541f0
jahids-web <jahiduddin922@gmail.com>
2024-04-08 17:45:09 +0600

    StackOverflow.Base.csproj

---
+++ StackOverflow/StackOverflow.Base/Migrations/20240408105230_ApiMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StackOverflow.Base.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApiMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
