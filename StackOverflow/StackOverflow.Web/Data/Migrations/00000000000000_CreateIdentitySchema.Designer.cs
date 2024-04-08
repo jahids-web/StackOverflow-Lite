f481a00d86be18b6119e25b3a664bf802bd47097
jahids-web <jahiduddin922@gmail.com>
2024-04-08 17:43:51 +0600

    Add Migrations folder

--- StackOverflow/StackOverflow.Web/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ StackOverflow/StackOverflow.Web/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using StackOverflow.Base.DataContext;
 using StackOverflow.Web.Data;
 using System;
 
