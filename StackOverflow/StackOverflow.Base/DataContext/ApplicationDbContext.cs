0cabf46c3e507987a712a4135b1746345e872f39
jahids-web <jahiduddin922@gmail.com>
2024-04-09 16:43:09 +0600

    Add New ApplicationDbContext

--- StackOverflow/StackOverflow.Base/DataContext/ApplicationDbContext.cs
+++ StackOverflow/StackOverflow.Base/DataContext/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace StackOverflow.Base.DataContext
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-           : base(options)
+            : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
         }
     }
 }
