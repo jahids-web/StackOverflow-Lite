7ac46bab899df6dd995a1585104b5d928b9024bc
jahids-web <jahiduddin922@gmail.com>
2024-04-17 20:42:12 +0600

    Add Entities Question

--- StackOverflow/StackOverflow.Base/Features/Questions/Entities/Question.cs
+++ StackOverflow/StackOverflow.Base/Features/Questions/Entities/Question.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace StackOverflow.Base.Features.Questions.Entities
 {
     public class Question
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? QuestionId { get; set; }
+
+        [Required]
+        public string? QuestionTitle { get; set; }
+
+        [Required]
+        public string? QuestionBody { get; set; }
+
     }
 }
