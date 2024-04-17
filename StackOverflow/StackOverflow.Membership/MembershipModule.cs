d30458e28df008fd58990709b5aaf59967129b77
jahids-web <jahiduddin922@gmail.com>
2024-04-17 12:05:29 +0600

    Add AutoFac

--- StackOverflow/StackOverflow.Membership/MembershipModule.cs
+++ StackOverflow/StackOverflow.Membership/MembershipModule.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
 
 namespace StackOverflow.Membership
 {
-    public class MembershipModule
+    public class MembershipModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            base.Load(builder);
+        }
     }
 }
