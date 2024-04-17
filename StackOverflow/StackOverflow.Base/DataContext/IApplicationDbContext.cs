92d24333fc08ddc2f019d7a095c4acf4b6197d1e
jahids-web <jahiduddin922@gmail.com>
2024-04-17 12:07:38 +0600

    Create IApplicationDbContext Class

---
+++ StackOverflow/StackOverflow.Base/DataContext/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using StackOverflow.Base.Features.Questions.Entities;
+using System.Data.Entity;
+
+namespace StackOverflow.Base.DataContext
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+    }
+}
