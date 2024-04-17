3f4a4dafa1ad4b6475824aaf6354cbe245e64d2b
jahids-web <jahiduddin922@gmail.com>
2024-04-17 12:07:08 +0600

    Add connectionString

--- StackOverflow/StackOverflow.Base/DataContext/ApplicationDbContext.cs
+++ StackOverflow/StackOverflow.Base/DataContext/ApplicationDbContext.cs
@@ -5,11 +5,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace StackOverflow.Base.DataContext
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser> , IApplicationDbContext
     {
+        private readonly string? _connectionString;
+        private readonly string? _migrationAssemblyName;
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public ApplicationDbContext(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -19,3 +28,5 @@ namespace StackOverflow.Base.DataContext
         }
     }
 }
+
+
