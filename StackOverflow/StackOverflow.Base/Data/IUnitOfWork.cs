99f64b219c5365b8cd24f1afdb03616bc22c96a9
jahids-web <jahiduddin922@gmail.com>
2024-04-19 23:32:25 +0600

    Add UnitOfWork

--- StackOverflow/StackOverflow.Base/Data/IUnitOfWork.cs
+++ StackOverflow/StackOverflow.Base/Data/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StackOverflow.Base.Features.Questions.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace StackOverflow.Base.Data
 {
     public interface IUnitOfWork
     {
+       public IQuestionRepository QuestionRepository { get; }
     }
 }
--- StackOverflow/StackOverflow.Base/Data/UnitOfWork.cs
+++ StackOverflow/StackOverflow.Base/Data/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StackOverflow.Base.DataContext;
+using StackOverflow.Base.Features.Questions.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,25 @@ namespace StackOverflow.Base.Data
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _context;
+      
+        public UnitOfWork(ApplicationDbContext context) 
+        { 
+            _context = context;
+        }
+
+        private IQuestionRepository? _questionRepository;
+        public IQuestionRepository QuestionRepository =>
+            _questionRepository ?? new QuestionRepository(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
     }
 }
